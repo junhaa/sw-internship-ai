cfc24a2e08c8a84c7d3330e77aa38eca36cfcd9e
junhaa <2171326@hansung.ac.kr>
2024-10-14 13:43:31 +0900

    :sparkles: FEAT. 단어 임베딩 저장 후 유사 문장 검색 기능 구현

--- main.py
+++ main.py
@@ -1,16 +1,30 @@
-# This is a sample Python script.
+from src.sentence_transform import sentence_embedding_save, isInit
+from src.search_sentences import search_similar_sentences
+import time
 
-# Press <no shortcut> to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+# 임베딩할 문장 리스트 (벡터 DB)
+sentences = [
+    "주로 많이 사용하는 AI 기반 서비스는 어떤게 있나요?",
+    "AI를 활용한 비즈니스 혁신에 관심이 있습니다.",
+    "회사의 데이터 분석 역량을 강화하고 싶습니다."
+]
+
+# 입력으로 들어온 문장 리스트
+query_sentences = [
+    "데이터 기반 분석 기능을 추가하고 싶어요.",
+    "많이 활용하는 AI를 사용하고 싶어요."
+]
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    # 초기화 대기
+    while(not isInit()):
+        time.sleep(1)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    # 저장할 문장 임베딩 후 저장
+    sentence_embedding_save(sentences)
+
+    # 저장된 문장 기반 FAISS 인덱스 탐색
+    search_similar_sentences(sentences, 1);
+
+
